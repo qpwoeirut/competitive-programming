386048cf4ba7ceaf5c2d57e22ec168aae4fbf3db
qpwoeirut <stanleyzhong8@gmail.com>
2020-09-19 19:32:51 -0700

    solved supertrees after reading editorial and thonking for 3 hours

--- ojuz/ioi/2020/Day1/supertrees/cpp/supertrees.cpp
+++ ojuz/ioi/2020/Day1/supertrees/cpp/supertrees.cpp
@@ -4,11 +4,13 @@
 using namespace std;
 
 typedef vector<vector<int>> vvi;
+typedef pair<int,int> pii;
 
 const int MN = 1001;
 int N;
 
 int root[MN];
+int oroot[MN];
 int par(int u) {
     if (u == root[u]) return u;
     root[u] = par(root[u]);
@@ -19,82 +21,107 @@ void join(int u, int v) {
     if (ru == rv) return;
     root[ru] = rv;
 }
+int opar(int u) {
+    if (u == oroot[u]) return u;
+    oroot[u] = opar(oroot[u]);
+    return oroot[u];
+}
+void ojoin(int u, int v) {
+    int ru = opar(u), rv = opar(v);
+    if (ru == rv) return;
+    oroot[ru] = rv;
+}
 bool dsu(const vvi& p) {
     for (int i=0; i<N; ++i) {
         root[i] = i;
+        oroot[i] = i;
     }
     for (int i=0; i<N; ++i) {
         for (int j=0; j<N; ++j) {
+            if (p[i][j] == 3) return 0;
             if (p[i][j]) join(i, j);
+            if (p[i][j] == 1) ojoin(i, j);
+        }
+    }
+    for (int i=0; i<N; ++i) {
+        for (int j=0; j<N; ++j) {
+            if (p[i][j] == 0 && par(i) == par(j)) return 0;
+            if (p[opar(i)][j] != p[i][j]) return 0;
         }
     }
     
-    for (int i=0; i<N; ++i) {
-        for (int j=0; j<N; ++j) {
-            if (par(i) == par(j) && p[i][j] == 0) {
-                return 0;
+    return 1;
+}
+
+int ones[MN];
+vvi ans;
+bool make_part(const vvi& p, const vector<int>& nodes) {
+    set<pii, greater<pii>> q;
+    for (int i=0; i<nodes.size(); ++i) {
+        q.insert(pii(ones[nodes[i]], nodes[i]));
+    }
+
+    int root = -1;
+    int last = -1;
+
+    int inner_ct = 0;
+    while (q.size() > 0) {
+        int cur = q.begin()->second;
+        q.erase(q.begin());
+        ++inner_ct;
+
+        if (root == -1) {
+            root = cur;
+        }
+        if (last != -1) {
+            ans[cur][last] = 1;
+            ans[last][cur] = 1;
+        }
+        last = cur;
+
+        for (int i=0; i<N; ++i) {
+            if (cur == i) continue;
+            if (p[cur][i] == 1) {
+                ans[cur][i] = 1;
+                ans[i][cur] = 1;
+                q.erase(pii(ones[i], i));
             }
         }
     }
-    return 1;
-}
-int sub2(const vvi& p) {
-    if (!dsu(p)) return 0;
-    vvi ans(N, vector<int>(N));
-    for (int i=0; i<N; ++i) {
-        if (i != par(i)) {
-            ans[i][par(i)] = true;
-            ans[par(i)][i] = true;
-        }
-        for (int j=0; j<N; ++j) assert(p[i][j] < 2);
-    }
-    build(ans);
-    return 1;
-}
-int sub3(const vvi& p) {
-    if (!dsu(p)) return 0;
-    vvi ans(N, vector<int>(N));
-    vvi groups;
-    for (int i=0; i<N; ++i) {
-        if (i == par(i)) {
-            vector<int> cur;
-            for (int j=0; j<N; ++j) {
-                if (i == par(j)) {
-                    cur.push_back(j);
-                }
-                assert(p[i][j] < 3);
+    assert(root != -1 && last != -1);
+    if (inner_ct <= 2) {
+        for (int i=0; i<N; ++i ) {
+            if (p[root][i] == 2 || p[last][i] == 2) {
+                return false;
             }
-            groups.push_back(cur);
-        }
-    }
-    for (int i=0; i<groups.size(); ++i) {
-        int gsz = groups[i].size();
-        if (gsz == 1) continue;
-        if (gsz == 2) return 0;
-        for (int j=0; j<gsz; ++j) {
-            ans[groups[i][(j+1) % gsz]][groups[i][j]] = ans[groups[i][j]][groups[i][(j+1) % gsz]] = 1;
         }
     }
 
-    build(ans);
+    if (root != last) {
+        ans[root][last] = 1;
+        ans[last][root] = 1;
+    }
     return 1;
 }
 
 int construct(std::vector<std::vector<int>> p) {
 	N = p.size();
+    if (!dsu(p)) return 0;
 
-    set<int> vals;
+    map<int, vector<int>> parts;
     for (int i=0; i<N; ++i) {
+        parts[par(i)].push_back(i);
         for (int j=0; j<N; ++j) {
-            vals.insert(p[i][j]);
+            if (p[i][j] == 1) ++ones[i];
         }
     }
 
-    if ((vals.size() == 1 && vals.count(1)) || (vals.size() == 2 && vals.count(0) && vals.count(1))) {
-        return sub2(p);
+    ans = vvi(N, vector<int>(N));
+    for (auto it=parts.begin(); it!=parts.end(); ++it) {
+        if (!make_part(p, it->second)) return 0;
     }
-    if (vals.count(2)) {
-        return sub3(p);
-    }
-    return 0;
+
+    build(ans);
+
+    return 1;
 }
