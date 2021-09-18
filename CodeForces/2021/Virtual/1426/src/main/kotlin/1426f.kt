27296ddeb6d13ed40442e3539608f803c9e7736c
qpwoeirut <stanleyzhong8@gmail.com>
2021-09-18 13:07:52 -0700

    move some resources around

---
+++ CodeForces/2021/Virtual/1426/src/main/kotlin/1426f.kt
@@ -0,0 +1,26 @@
+import kotlin.*
+import kotlin.math.*
+
+private fun readInt() = readLine()!!.toInt() // single int
+private fun readLong() = readLine()!!.toLong() // single long
+private fun readDouble() = readLine()!!.toDouble() // single double
+
+private fun readStrings() = readLine()!!.split(" ") // list of strings
+private fun readInts() = readStrings().map { it.toInt() } // list of ints
+private fun readLongs() = readStrings().map { it.toLong() } // list of long
+private fun readDoubles() = readStrings().map { it.toDouble() } // list of double
+
+private val MN = 1e6.toInt() + 6
+private val MOD = 1e9.toInt() + 7
+
+private fun solve() {
+
+}
+
+fun main() {
+//    val T = 1
+    val T = readInt()
+    repeat(T) {
+        solve()
+    }
+}
