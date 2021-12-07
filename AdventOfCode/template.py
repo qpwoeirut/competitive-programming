d4947f2f5db3cef8d90c4af687096ea8274dceca
qpwoeirut <stanleyzhong8@gmail.com>
2021-12-06 21:07:51 -0800

    Update template to include input parsing for an array of numbers

--- AdventOfCode/template.py
+++ AdventOfCode/template.py
@@ -7,6 +7,7 @@ ans2 = 0
 with open("input.txt") as file:
     # A = [line.strip() for line in file]
     A = [int(line.strip()) for line in file]
+    # A = list(map(int, file.readline().split(',')))
 
 N = len(A)
 print("N =", N)
