17a825b4804025403bd2e0f45c8b2f7060787326
qpwoeirut <stanleyzhong8@gmail.com>
2021-12-10 22:47:46 -0800

    Add list for adjacent cells in a grid

--- AdventOfCode/template.py
+++ AdventOfCode/template.py
@@ -5,6 +5,10 @@
 ans = 0
 ans2 = 0
 
+#      N   E   S   W
+chr = [-1, 0,  1,  0, -1, -1,  1,  1]
+chc = [0,  1,  0, -1, -1,  1, -1,  1]
+
 with open("input.txt") as file:
     # A = list(map(int, file.readline().split(',')))
     A = [int(line.strip()) for line in file]
