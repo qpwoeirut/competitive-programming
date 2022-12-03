4c58700fc370a5d8bd2ee5d11d34ae20a6774c4d
qpwoeirut <stanleyzhong8@gmail.com>
2022-12-02 21:21:31 -0800

    Parse problem page to get sample input

--- AdventOfCode/setup_day.py
+++ AdventOfCode/setup_day.py
@@ -1,5 +1,6 @@
 # from https://github.com/ytchang05/AdventOfCode/blob/main/initialize.py
 
+import bs4
 import os
 import requests
 import time
@@ -25,7 +26,6 @@ if not os.path.exists(f"{year}/day{day}"):
 # Create new Python file for the day
 with open(f"{year}/day{day}/day{day}.py", "w") as f, open("template.py") as template:
     f.write(template.read().replace("{{year}}", year).replace("{{day}}", day))
-open(f"{year}/day{day}/sample.txt", "w+").close()
 
 # Wait for midnight EST
 now = est_now()
@@ -39,4 +39,12 @@ print("Advent of Code started!")
 
 # Download input txt file from AoC website
 with open(f"{year}/day{day}/input.txt", "w") as f:
-    f.write(requests.get(f"https://adventofcode.com/{year}/day/{day}/input", cookies=cookies).content.decode())
+    f.write(requests.get(f"https://adventofcode.com/{year}/day/{day}/input", cookies=cookies).content.decode().strip())
+
+# Parse page and grab the largest <code> element, which we'll assume is the sample
+page = requests.get(f"https://adventofcode.com/{year}/day/{day}", cookies=cookies).content.decode()
+soup = bs4.BeautifulSoup(page, features="html.parser")
+codes = [code.text.strip() for code in soup.find_all("code")]
+sample = max(codes, key=len)
+with open(f"{year}/day{day}/sample.txt", "w+") as sample_file:
+    sample_file.write(sample)
