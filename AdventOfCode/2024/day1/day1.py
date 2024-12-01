de7bb689cea75ee6a2a666d33e496cdbeb9622f9
qpwoeirut <stanleyzhong8@gmail.com>
2024-11-30 23:04:37 -0600

    Add AoC Day 1

---
+++ AdventOfCode/2024/day1/day1.py
@@ -0,0 +1,62 @@
+# Forgot to update .token file...
+
+from submit_answer import submit_answer
+
+
+#      N   E   S   W
+chr = [-1, 0,  1,  0, -1, -1,  1,  1]
+chc = [0,  1,  0, -1, -1,  1, -1,  1]
+
+
+LEVEL = 2
+SAMPLE_ANSWERS = [11, 31]
+SAMPLE_ANSWER = SAMPLE_ANSWERS[LEVEL - 1]
+
+
+def solve(input_string: str) -> int or str:
+    # A = list(input_string)
+    # A = list(map(int, input_string.split(',')))
+    # A = [line for line in input_string.split('\n')]
+    # A = [int(line) for line in input_string.split('\n')]
+    A = [list(map(int, line.split())) for line in input_string.split('\n')]
+
+    N = len(A)
+    print("N =", N)
+    print(A[:10])
+    # M = len(A[0])
+
+    l1 = list(map(lambda t: t[0], A))
+    l2 = list(map(lambda t: t[1], A))
+    l1.sort()
+    l2.sort()
+
+    ans1 = 0
+    ans2 = 0
+    for i in range(N):
+        ans1 += abs(l2[i] - l1[i])
+        ans2 += l1[i] * l2.count(l1[i])
+
+    # for i in range(N):
+
+    if LEVEL == 1:
+        return ans1
+    else:
+        return ans2
+
+
+def main():
+    with open("sample.txt") as sample_file:
+        sample_input = sample_file.read().strip('\n')
+    sample_answer = solve(sample_input)
+    print("Answer for sample:", sample_answer)
+    assert sample_answer == SAMPLE_ANSWER and sample_answer is not None, f"Got {sample_answer} instead of {SAMPLE_ANSWER}"
+
+    with open("input.txt") as input_file:
+        inp = input_file.read().strip('\n')
+    answer = solve(inp)
+    print("Answer:", answer)
+    assert submit_answer(2024, 1, LEVEL, answer) is True
+
+
+if __name__ == '__main__':
+    main()
